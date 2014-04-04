9780cdc892901b889e366f59bbe61b27a3ce22a5
magrando <magrando@alumnos.ubiobio.cl>
2014-04-04 10:55:20 -0400

    Línea agregada por web modificada.

--- tallerGit/src/tallerGit/Main.java
+++ tallerGit/src/tallerGit/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Segundo comit");
-		System.out.println("Linea agregada por web");
+		System.out.println("Linea agregada por web, nuevamente modificada");
 
 	}
 
